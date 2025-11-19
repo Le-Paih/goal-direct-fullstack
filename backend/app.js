cfe90b5a140aeb54cfbc9883499c38be4168d0f0
Le-Paih <luis.lepaih@gmail.com>
2025-11-19 20:59:41 +0000

    Updating CORS

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(
   cors({
     origin: [
-      'http://127.0.0.1:5000',
+      // 'http://127.0.0.1:5000',
       'https://goal-direct-fullstack.vercel.app/',
     ],
     credentials: true,
