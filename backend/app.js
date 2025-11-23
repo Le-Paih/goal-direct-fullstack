536052ec09601c108bad69e956ff2dcf3b02277e
Le-Paih <luis.lepaih@gmail.com>
2025-11-23 17:52:49 +0000

    Updating images

--- backend/app.js
+++ backend/app.js
@@ -94,7 +94,7 @@ app.use((req, res) => {
 // Serving static files
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.use('/img', express.static(path.join(__dirname, '/backend/public/img')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
 // app.use('/BootImgs', express.static(path.join(__dirname, 'public/BootImgs')));
 
 app.get('*', (req, res) => {
